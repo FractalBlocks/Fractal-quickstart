26d3dde63881561698dfcff2e62d05e61f54a29e
Carlos Galarza <carloslfu@gmail.com>
2017-03-16 12:39:17 -0500

    Include webpack-env typings

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,11 @@
 import { run } from 'fractal-core'
 import { viewHandler } from 'fractal-core/interfaces/view'
 import { styleHandler } from 'fractal-core/groups/style'
+import { mergeStates } from 'fractal-core/utils/reattach' // DEV
 import { warn, error } from 'fractal-core/utils/log' // DEV
 import root from './root'
 
-run({
+const app = run({
   root,
   groups: {
     style: styleHandler(),
@@ -15,3 +16,11 @@ run({
   warn,
   error,
 })
+
+// Hot reload - DEV ONLY
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    let m = require('./app').default
+    app.moduleAPI.reattach(m, mergeStates)
+  })
+}
