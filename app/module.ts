ddd8ed64ec02bf3079456b9d13ef0c3ce82e54ea
Carlos Galarza <carloslfu@gmail.com>
2017-07-25 19:34:14 -0500

    Add RunModule type

--- app/module.ts
+++ app/module.ts
@@ -3,11 +3,12 @@ import {
   Component,
   // DEV
   logFns,
+  RunModule,
 } from 'fractal-core'
 import { viewHandler } from 'fractal-core/interfaces/view'
 import { styleHandler } from 'fractal-core/groups/style'
 
-export const runModule = (root: Component<any>, DEV: boolean, viewCb?)=> run({
+export const runModule: RunModule = (root: Component<any>, DEV: boolean, viewCb?)=> run({
   root,
   groups: {
     style: styleHandler('', DEV),
