ac7f76e29a2bd531da6598aef6e3a84b55fb1b85
Carlos Galarza <carloslfu@gmail.com>
2017-09-12 09:29:05 -0500

    Fix fusebox config

--- fuse.js
+++ fuse.js
@@ -1,61 +1,3 @@
-// const {
-//   FuseBox,
-//   SVGPlugin,
-//   CSSPlugin,
-//   BabelPlugin,
-//   QuantumPlugin,
-//   WebIndexPlugin,
-//   EnvPlugin,
-//   Sparky,
-// } = require('fuse-box')
-
-// let fuse, app, vendor, isProduction
-
-// Sparky.task('config', () => {
-//   fuse = new FuseBox({
-//     homeDir: 'app/',
-//     sourceMaps: !isProduction,
-//     hash: isProduction,
-//     output: 'dist/$name.js',
-//     useTypescriptCompiler: true,
-//     experimentalFeatures: true,
-//     plugins: [
-//       SVGPlugin(),
-//       CSSPlugin(),
-//       EnvPlugin({ isProduction }),
-//       WebIndexPlugin({
-//         path: '.',
-//         template: 'app/index.html'
-//       }),
-//       isProduction && QuantumPlugin({
-//         treeshake: true,
-//         uglify: true
-//       })
-//     ]
-//   })
-//   // vendor
-//   vendor = fuse.bundle('vendor').instructions('~ index.ts')
-
-//   // bundle app
-//   app = fuse.bundle('app').instructions('> [index.ts]')
-// })
-
-// Sparky.task('default', ['clean', 'config'], () => {
-//   fuse.dev({ port: 3000 })
-//   // add dev instructions
-//   app.watch().hmr()
-//   return fuse.run()
-// })
-
-// Sparky.task('clean', () => Sparky.src('dist/').clean('dist/'))
-// Sparky.task('prod-env', ['clean'], () => { isProduction = true })
-// Sparky.task('dist', ['prod-env', 'config'], () => {
-//   // comment out to prevent dev server from running (left for the demo)
-//   fuse.dev({ port: 3000 })
-//   return fuse.run()
-// })
-
-
 const {
   FuseBox,
   SassPlugin,
@@ -108,10 +50,10 @@ Sparky.task('config', () => {
   })
 
   // vendor
-  let vendor = fuse.bundle('vendor').instructions('~ index.ts')
+  vendor = fuse.bundle('vendor').instructions('~ index.ts')
 
   // bundle app
-  let app = fuse.bundle('app').instructions('> [index.ts]')
+  app = fuse.bundle('app').instructions('> [index.ts]')
 
 })
 
